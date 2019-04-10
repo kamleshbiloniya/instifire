53dfec4ec6df8840abe048c24962cd21d7088363
Ankit Kumar Singh <ankitaks@iitk.ac.in>
2019-04-10 21:31:15 +0530

    rem comments

--- src/app/app.firebaseconfig.ts
+++ src/app/app.firebaseconfig.ts
@@ -1,11 +1,3 @@
-// export const FIREBASE_CONFIG_kamlesh = {
-//     apiKey: "AIzaSyAESDDOnLGOFmUs3YUcMG2HrjaL5NwEoZE",
-//     authDomain: "instifire-f0c21.firebaseapp.com",
-//     databaseURL: "https://instifire-f0c21.firebaseio.com",
-//     projectId: "instifire-f0c21",
-//     storageBucket: "instifire-f0c21.appspot.com",
-//     messagingSenderId: "624946498868"
-// }
 export const FIREBASE_CONFIG = {
     apiKey: "AIzaSyAsgW_-OQBb8yFS6eC6g-n3kWDdcL5ztSw",
     authDomain: "instantifire.firebaseapp.com",
