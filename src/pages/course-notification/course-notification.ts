0b0b515a732650a05884bb2229ef62bba3b0871c
Ankit Kumar Singh <ankitaks@iitk.ac.in>
2019-04-10 12:24:32 +0530

    Migrated DB for course-notification

--- src/pages/course-notification/course-notification.ts
+++ src/pages/course-notification/course-notification.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
 import { AngularFireDatabase } from 'angularfire2/database'
 import { AngularFirestore } from 'angularfire2/firestore';
+import { empty } from 'rxjs';
 /**
  * Generated class for the CourseNotificationPage page.
  *
@@ -23,10 +24,33 @@ export class CourseNotificationPage {
   data:any[];
   noti :any;
   notifications: any=[];
+  weekday = {
+  0 :  "Sunday",
+  1 : "Monday",
+  2 : "Tuesday",
+  3 : "Wednesday",
+  4 : "Thursday",
+  5 : "Friday",
+  6 : "Saturday"
+  }
+  month ={
+    0 : "January",
+    1 : "February",
+    2 : "March",
+    3 : "April",
+    4 : "May",
+    5 : "June",
+    6 : "July",
+    7 : "August",
+    8 : "September",
+    9 : "October",
+    10 : "November",
+    11 : "December"
+  }
+  emptyMsg =false;
   // itemRef:AngularFireDatabase.database.ref;
   constructor(public navCtrl: NavController, 
     public navParams: NavParams,
-    private db:AngularFireDatabase,
     private fireStore:AngularFirestore,
     private alertCtrl: AlertController
     ) {
@@ -40,7 +64,28 @@ export class CourseNotificationPage {
       let payload = '/college/'+this.collegeId+'/'+this.courseNo+'/'+this.courseNo;
       console.log('payload',payload)
       this.fireStore.doc(payload).valueChanges().subscribe(data=>{
-        console.log('data_c_n:',data)
+        this.creator = data['creator']
+        console.log('data_c_n:',this.creator)
+        Object.assign(this.notifications,data['notification'])
+        this.notifications.sort((a, b) => a['from'] <= b['from'] ? -1 : 1);
+        for(let i = 0; i< this.notifications.length;i++){
+          this.notifications[i]['expanded'] = false
+          let tmp =''
+          let from = this.notifications[i]['from'].toDate()
+          console.log('f---------',from)
+          tmp = this.weekday[from.getDay()]+'\t'+from.getDate()+'\t'+this.month[from.getMonth()]+'\t'+from.getFullYear()
+          this.notifications[i]['from'] =tmp
+          from =  this.notifications[i]['upto'].toDate()
+          tmp = this.weekday[from.getDay()]+'\t'+from.getDate()+'\t'+this.month[from.getMonth()]+'\t'+from.getFullYear()
+          this.notifications[i]['upto'] =tmp
+        }
+        if (this.notifications.length == 0){
+          this.emptyMsg =true;
+        }else{
+          this.emptyMsg = false;
+        }
+        // console.log(this.notifications.length)
+
       })
       // db.list(payload).valueChanges().subscribe(data=>{
       //   console.log('data...........',data);
@@ -132,9 +177,9 @@ export class CourseNotificationPage {
     this.notifications.map((listItem) => {
 
         if(item == listItem){
-            listItem.expanded = !listItem.expanded;
+            listItem['expanded'] = !listItem['expanded'];
         } else {
-            listItem.expanded = false;
+            listItem['expanded'] = false;
         }
 
         return listItem;
