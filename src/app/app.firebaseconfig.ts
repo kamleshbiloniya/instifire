a50117303d47208ede3ad4fb542213db30b37d9b
kamleshbiloniya <kamlesh@iitk.ac.in>
2019-06-28 22:37:33 +0530

    credentials removed

--- src/app/app.firebaseconfig.ts
+++ src/app/app.firebaseconfig.ts
@@ -1,8 +1,8 @@
 export const FIREBASE_CONFIG = {
-    apiKey: "AIzaSyAsgW_-OQBb8yFS6eC6g-n3kWDdcL5ztSw",
-    authDomain: "instantifire.firebaseapp.com",
-    databaseURL: "https://instantifire.firebaseio.com",
-    projectId: "instantifire",
-    storageBucket: "instantifire.appspot.com",
-    messagingSenderId: "705136785052"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: ""
 }
